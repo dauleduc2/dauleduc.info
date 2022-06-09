fda6be1a558cc0cb6b3a0aac812912fbc44ea0bb
Duc Dauuu <dauleduc2@gmail.com>
2022-06-09 13:33:47 +0700

    update form

--- src/pages/components/AboutMe/index.tsx
+++ src/pages/components/AboutMe/index.tsx
@@ -52,7 +52,9 @@ const AboutMe: React.FunctionComponent<AboutMeProps> = ({
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                 </svg>
               </div>
-              <div className="text-[13px] lg:text-xl">dauleduc2@gmail.com</div>
+              <div className="text-[13px] lg:text-xl">
+                https://www.linkedin.com/in/ducdauu/
+              </div>
             </div>
             <div className="flex items-center space-x-3">
               <div className="w-4 h-4 text-black duration-300 cursor-pointer lg:w-8 lg:h-8 hover:scale-125">
@@ -90,10 +92,7 @@ const AboutMe: React.FunctionComponent<AboutMeProps> = ({
             <h2 className="text-base font-bold uppercase lg:text-2xl">
               LANGUAGES
             </h2>
-            <p className="text-sm lg:text-xl">
-              FPT University HCM | Bachelor Degree 09/2019 - Present 3.35/4(
-              Semester 4/9)
-            </p>
+            <p className="text-sm lg:text-xl">ACADEMIC PREPARATORY ENGLISH</p>
           </div>
         </div>
       </div>
